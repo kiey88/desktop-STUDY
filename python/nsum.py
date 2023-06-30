0b9468c7afcc0b1a4ff62f598240915e30061390
kiey88 <133174359+kiey88@users.noreply.github.com>
2023-06-30 18:00:30 +0900

    ict 폴리텍대학

---
+++ python/nsum.py
@@ -0,0 +1,5 @@
+def nsum(num):
+    total = 0
+    for i in range(1, num + 1):
+        total += i
+    return total
---
+++ python/oper.py
@@ -0,0 +1,5 @@
+def evenodd(num) :
+    if num%2 == 0 :
+        print("짝수")
+    else : 
+        print("홀수")
