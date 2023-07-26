7a3a6355f27a17331d0c49434cf8b867c3161892
kiey88 <133174359+kiey88@users.noreply.github.com>
2023-07-26 18:39:42 +0900

    ict폴리텍대학

---
+++ javascript/condition.js
@@ -0,0 +1,73 @@
+// const score = 90;
+// if(score >= 90){
+//     console.log("A");
+// } else if (score < 90 && score >=80){
+//     console.log("B");
+// } else if (score < 80 && score >=70){
+//     console.log("C");
+// } else if (score < 70 && score >=60){
+//     console.log("D");
+// } else {
+//     console.log("F");
+// }
+
+// // 중첩 if문
+// if(true){
+//     console.log("참입니다");
+//     if(false){
+//         console.log("참입니다");
+//     } else {
+//         console.log("거짓입니다");
+//     }
+// }
+
+// switch case문
+// 입력값이 사과 바나나 배이면 과일 출력
+// 입력값이 토마토이면 채소 출력
+// 그 이외의 값은 다시 입력하세요 출력
+
+// if(value === "사과"){
+//     console.log("과일입니다.");
+// } else if(value === "바나나"){
+//     console.log("과일입니다.");
+// } else if(value === "배"){
+//     console.log("과일입니다.");
+// } else{
+//     console.log("다시입력하세요.");
+// }
+
+let value = "사과"
+// switch(value){
+//     case "바나나":
+//         console.log("과일입니다");
+//         break;
+//     case "사과":
+//         console.log("과일입니다");
+//         break;
+//     case "딸기":
+//         console.log("과일입니다");
+//         break;
+//     case "토마토":
+//         console.log("채소입니다");
+//         break;
+//     default :
+//         console.log("다시입력하세요");
+//         break;
+// }
+// switch(value){
+//     case "바나나":
+//     case "사과":        
+//     case "딸기":
+//         console.log("과일입니다");
+//         break;
+//     case "토마토":
+//         console.log("채소입니다");
+//         break;
+//     default :
+//         console.log("다시입력하세요");
+//         break;
+// }
+
+// 삼항연산자
+let data = (6 > 4) ? "참입니다" : "거짓입니다"
+console.log(data);
