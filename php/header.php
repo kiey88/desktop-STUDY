cffb5106c73725fbe8999a48464411ea59f3b361
kiey88 <133174359+kiey88@users.noreply.github.com>
2023-09-05 18:08:00 +0900

    study

--- php/header.php
+++ php/header.php
@@ -6,7 +6,29 @@
     <title>Document</title>
 </head>
 <body>
-    <a href="member_form.php">회원 가입</a>
-    <a href ="login_form.php">로그인</a>
+    <?php session_start();
+        if(isset($_SESSION["userid"])) $userid = $_SESSION["userid"]; else $userid = "";
+        if(isset($_SESSION["username"])) $username = $_SESSION["username"]; else $username = "";
+        if(isset($_SESSION["userlevel"])) $userlevel = $_SESSION["userlevel"]; else $userlevel = "";
+        if(isset($_SESSION["userpoint"])) $userpoint = $_SESSION["userpoint"]; else $userpoint = "";
+    ?>
+    <div>
+        <?php
+            if(!$userid) {
+        ?>
+        <?php
+            } 
+        <a href="member_form.php">회원 가입</a>
+        |
+        <a href ="login_form.php">로그인</a>
+        else {
+            $logged = $username."(".$userid.")님[Level:".$userlevel.",point:".$userpoint."]";
+        ?>
+            <?= $logged ?> | <a href="logout.php"> 로그아웃 </a> |
+            <a href="member_modify_form.php"> 정보수정 </a> <br>
+    </div> <br>
+        <?php
+        }
+        ?>
 </body>
 </html>
---
+++ php/login_form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <meta charset="utf-8">
+    <title>회원 로그인</title>
+    <script type="text/javascript"> function check_input() {
+        if (!document.login_form.id.value) { alert("아이디를 입력하세요"); document.login_form.id.focus(); return;
+        }
+        if (!document.login_form.pass.value) { alert("비밀번호를 입력하세요"); document.login_form.pass.focus(); return;
+        }
+        document.login_form.submit();
+    }
+</script>
+
+</head>
+<body>
+    <header> <?php include "header.php";?></header>
+    <hr>
+    <section>
+    <form	name="login_form"  method="post"  action="Login.php">
+        <p>
+            아이디 : <input type="text" name="id"><br>
+            비밀번호 : <input type="password" name="pass"><br>
+            <input  type="button"  value="Click"  oncLick="check_input()">
+        </p>
+    </form>
+    </section>
+    <hr>
+    <footer>
+        <?php include "footer.php";?>
+    </footer>
+</body>
+</html>
